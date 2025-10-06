31b9033342d36cc5b23245e48851a5d70e556f0e
Robin <Robin2.Bareham@live.uwe.ac.uk>
2025-10-06 12:52:07 +0100

    Completed Basics of Worksheet1

---
+++ Assets/GEP Core/Scripts/Game Manager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameState { GAMEPLAY,PAUSE}
+    public GameState state = GameState.GAMEPLAY;
+    public bool hasChangedState = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        state = GameState.GAMEPLAY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == GameState.GAMEPLAY)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                hasChangedState = true;
+                state = GameState.PAUSE;
+            }
+        }
+        else if (state == GameState.PAUSE) 
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                hasChangedState = true;
+                state = GameState.GAMEPLAY;
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (hasChangedState) 
+        {
+            hasChangedState = false;
+            if (state == GameState.GAMEPLAY) 
+            {
+                Time.timeScale = 1.0f;
+            }
+            if(state == GameState.PAUSE)
+            {
+                Time.timeScale = 0.0f;
+            }
+        }
+    }
+}
