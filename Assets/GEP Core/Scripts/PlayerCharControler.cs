bd485bd009b17d27efa04b6a046c7ff74a885f7e
Robin-Bareham <Robin2.Bareham@live.uwe.ac.uk>
2025-11-24 11:36:09 +0000

    Added input map

---
+++ Assets/GEP Core/Scripts/PlayerCharControler.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using StarterAssets;
+using UnityEngine.InputSystem;
+
+public class PlayerCharControler : ThirdPersonController
+{
+
+
+    private void OnPause(InputValue value)
+    {
+        if (value.isPressed)
+        { gameManager.pauseInput(); }
+    }
+    private void OnInventory(InputValue value)
+    {
+        if (value.isPressed)
+        { gameManager.inventoryInput(); }
+    }
+}
